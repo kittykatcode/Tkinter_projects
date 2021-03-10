f7d8b02392607df811d4ca8d3b4e7718622e2ce5
kat K <katcode.py@gmail.com>
2021-03-10 16:20:01 +0530

    databse.py got updated

--- ProjectFiles/data_base.py
+++ ProjectFiles/data_base.py
@@ -23,6 +23,102 @@ c= con.cursor()
 #    Zip_code integer)""")
 #create submit function
 
+# function to edit the database
+
+def update_record():
+    con = sqlite3.connect('Address_book.db')
+    # create the cursoor.. cursor is the one which connects to the database and does stuff for us
+    c= con.cursor()
+
+    record_ID = delete_box.get()
+
+    c.execute(""" UPDATE addressess SET
+
+        first_name = :first,
+        second_name = :s_name,
+        street_name = :address,
+        city = :city,
+        Zip_code = :z_code
+
+        WHERE oid = :oid """,
+        {
+        'first': f_name_editor.get(),
+        's_name': s_name_editor.get(),
+        'address': address_editor.get(),
+        'city': city_editor.get(),
+        'z_code': z_code_editor.get(),
+
+        'oid' : record_ID
+        })
+
+    # commit changes to database
+    con.commit()
+
+    #end /close database connection
+    con.close()
+
+    editor.destroy()
+
+
+def edit_record():
+    global editor
+    editor= Tk()
+    editor.title('Edit DataBase')
+    editor.geometry('400x200')
+
+    # Create a Database or connect to one
+    con = sqlite3.connect('Address_book.db')
+    # create the cursoor.. cursor is the one which connects to the database and does stuff for us
+    c= con.cursor()
+
+    record_ID = delete_box.get()
+
+    #inserting into database table
+    c.execute("SELECT * FROM addressess WHERE oid = " + record_ID)
+    records = c.fetchall()
+    
+    #create global fun to be used in update records
+    global f_name_editor
+    global s_name_editor
+    global address_editor
+    global city_editor
+    global z_code_editor
+
+    
+    #Create Text Boxes
+    f_name_editor = Entry(editor, width=30)
+    f_name_editor.grid(row=0,column=1, padx=10, pady=(10,0))
+    s_name_editor = Entry(editor, width=30)
+    s_name_editor.grid(row=1,column=1, padx=10)
+    address_editor = Entry(editor, width=30)
+    address_editor.grid(row=2,column=1, padx=10)
+    city_editor = Entry(editor, width=30)
+    city_editor.grid(row=3,column=1, padx=10)
+    z_code_editor = Entry(editor, width=30)
+    z_code_editor.grid(row=4,column=1, padx=10)
+    
+    #Create lables
+    Label(editor,text='First Name').grid(row=0,column=0,pady=(10,0))
+    Label(editor,text='second Name').grid(row=1,column=0)
+    Label(editor,text='Street Name').grid(row=2,column=0)
+    Label(editor,text='City ').grid(row=3,column=0)
+    Label(editor,text='Zip Code').grid(row=4,column=0)
+
+    # loop through results
+    for record in records:
+        f_name_editor.insert(0,record[0])
+        s_name_editor.insert(0,record[1])
+        address_editor.insert(0,record[2])
+        city_editor.insert(0,record[3])
+        z_code_editor.insert(0,record[4])
+
+
+    #Create submit button
+    Button(editor,text='save Changes', command=update_record, bd=2).grid(row=5,column=0,columnspan=2, padx=20,ipadx=100)
+
+
+
+
 #function to delete record
 def delete_record():
     # Create a Database or connect to one
@@ -78,14 +174,14 @@ def query_btn():
     #inserting into database table
     c.execute("SELECT *,oid FROM addressess")
     records = c.fetchall()
-    #print(records)
+    print(records)
     # looping through the result 
     print_rec = ''
     for record in records:
-        print_rec += str(record[5])+'\t'+str(record[0])+' '+str(record[1])+'\n' 
+        print_rec += str(record[0])+' '+str(record[1])+ '\t'+ str(record[5])+'\n' 
 
     Lable_record=Label(root, text= print_rec)
-    Lable_record.grid(row=10,column=0,columnspan=2, sticky=W)
+    Lable_record.grid(row=10,column=0,columnspan=2)
 
 
 
@@ -117,7 +213,7 @@ Label(root,text='second Name').grid(row=1,column=0)
 Label(root,text='Street Name').grid(row=2,column=0)
 Label(root,text='City ').grid(row=3,column=0)
 Label(root,text='Zip Code').grid(row=4,column=0)
-Label(root,text='ID number').grid(row=7,column=0)
+Label(root,text=' Select ID').grid(row=7,column=0)
 
 
 #Create submit button
@@ -127,6 +223,10 @@ Button(root,text='add Data to the database', command=submit, bd=2).grid(row=5,co
 Button(root,text='Show Records', command=query_btn,bd=2).grid(row=6,column=0,columnspan=2 ,ipadx=137)
 #button for deleting records
 Button(root, text='Delete Records', command=delete_record, bd=2).grid(row=8,column=0,columnspan=2,ipadx=136)
+
+#create Edit Button
+Button(root, text='Edit Records', command=edit_record, bd=2).grid(row=9,column=0,columnspan=2,ipadx=145)
+
 # commit changes to database
 con.commit()
 
